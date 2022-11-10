63137d4f9a4a820674131d600c4d63355d1c8b3b
Italo Garcia <italo@landgorilla.com>
2022-11-09 21:21:39 -0500

    modify variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,116 +1,109 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  const productsDOM = document.querySelector(".card-layout");
+  const categoriasDOM = document.querySelector(".categories-nav");
 
-const productsDOM = document.querySelector('.card-layout');
-const categoriasDOM = document.querySelector('.categories-nav');
-getCategories();
-getProducts();
+  getCategories();
+  getProducts();
 
-function getCategories() {
+  $("#findProduct").on("click", function (e) {
+    e.preventDefault();
+    var label = $("#labelProduct").val();
     $.ajax({
-        method: "POST",
-        url: "index.php",
-        data: {
-            "controlador": "categoria",
-            "accion": 'getAll'
-        }
-    }).done(function(product) {
-        json = JSON.parse(product);
-        let result = "";
+      method: "GET",
+      url: "index.php",
+      data: {
+        controlador: "producto",
+        accion: "findProduct",
+        text: label,
+      },
+    }).done(function (response) {
+      let result = "";
+      response.forEach((products) => {
         result += ` 
+        <div class="card-layout__item">
+            <a class="card">
+                <div class="thumb" style="background-image: url(${products.url_image});"></div>
+                <article>
+                <h4>${products.name}</h4>
+                <span>Price: $ ${products.price}</span>
+                <span>Discount: ${products.discount}</span>
+                </article>
+            </a>
+        </div>
+        `;
+      });
+      productsDOM.innerHTML = result;
+    });
+  });
+  console.log("ready!");
+  function getProducts() {
+    $.ajax({
+      method: "GET",
+      url: "index.php",
+      data: {
+        controlador: "producto",
+        accion: "getAll",
+      },
+    }).done(function (response) {
+      let result = "";
+      response.forEach((products) => {
+        result += ` 
+        <div class="card-layout__item">
+            <a class="card">
+                <div class="thumb" style="background-image: url(${products.url_image});"></div>
+                <article>
+                <h4>${products.name}</h4>
+                <span>Price: $ ${products.price}</span>
+                <span>Discount: ${products.discount}</span>
+                </article>
+            </a>
+        </div>
+        `;
+      });
+      productsDOM.innerHTML = result;
+    });
+  }
+  function getCategories() {
+    $.ajax({
+      method: "GET",
+      url: "index.php",
+      data: {
+        controlador: "categoria",
+        accion: "getAll",
+      },
+    }).done(function (response) {
+      let result = "";
+      result += ` 
             <li class="nav-item ">
                 <a class="nav-link" href="#" onclick="getProducts()">All</a>
             </li>
                       `;
-        json.forEach((categories) => {
-            result += ` 
+      response.forEach((categories) => {
+        result += ` 
             <li class="nav-item ">
                 <a class="nav-link" href="#" onclick="getProductforCategory(${categories.id})">${categories.name}</a>
             </li>
                     `;
-        });
+      });
 
-        categoriasDOM.innerHTML = result;
+      categoriasDOM.innerHTML = result;
     });
-}
-
-function getProducts() {
-    $.ajax({
-        method: "POST",
-        url: "index.php",
-        data: {
-            "controlador": "producto",
-            "accion": 'getAll'
-        }
-    }).done(function(product) {
-        json = JSON.parse(product);
-        let result = "";
-        json.forEach((products) => {
-            result += ` 
-        <div class="card-layout__item">
-            <a class="card">
-                <div class="thumb" style="background-image: url(${products.url_image});"></div>
-                <article>
-                <h4>${products.name}</h4>
-                <span>Price: $ ${products.price}</span>
-                <span>Discount: ${products.discount}</span>
-                </article>
-            </a>
-        </div>
-        `;
-        });
-        productsDOM.innerHTML = result;
-    });
-}
-$('#findProduct').on('click', function(e) {
-    e.preventDefault();
-    var label = $('#labelProduct').val();
-    $.ajax({
-        method: "POST",
-        url: "index.php",
-        data: {
-            "controlador": "producto",
-            "accion": 'findProduct',
-            "text": label
-        }
-    }).done(function(product) {
-        json = JSON.parse(product);
-        let result = "";
-        json.forEach((products) => {
-            result += ` 
-        <div class="card-layout__item">
-            <a class="card">
-                <div class="thumb" style="background-image: url(${products.url_image});"></div>
-                <article>
-                <h4>${products.name}</h4>
-                <span>Price: $ ${products.price}</span>
-                <span>Discount: ${products.discount}</span>
-                </article>
-            </a>
-        </div>
-        `;
-        });
-        productsDOM.innerHTML = result;
-    });
+  }
 });
-
 function getProductforCategory(id) {
-    $.ajax({
-        method: "POST",
-        url: "index.php",
-        data: {
-            "controlador": "categoria",
-            "accion": 'getProductforCategory',
-            "id": id
-        }
-    }).done(function(product) {
-
-        json = JSON.parse(product);
-        let result = "";
-        console.log(json);
-
-        json.forEach((products) => {
-
-            result += ` 
+  const productsDOM = document.querySelector(".card-layout");
+  $.ajax({
+    method: "GET",
+    url: "index.php",
+    data: {
+      controlador: "categoria",
+      accion: "getProductforCategory",
+      id: id,
+    },
+  }).done(function (response) {
+    let result = "";
+    response.forEach((products) => {
+      result += ` 
         <div class="card-layout__item">
             <a href="#" class="card">
                 <div class="thumb" style="background-image: url(${products.url_image});"></div>
@@ -122,9 +115,7 @@ function getProductforCategory(id) {
             </a>
         </div>
         `;
-        });
-        productsDOM.innerHTML = result;
     });
+    productsDOM.innerHTML = result;
+  });
 }
-console.log( "ready!" );
-});
